4febfe24070d08db4bc6f3b0b7e5f02cfa7e8068
jkemppinen <julia.kemppinen@gmail.com>
2021-11-02 13:41:19 +0100

    additions

--- scripts/fig3.R
+++ scripts/fig3.R
@@ -5,8 +5,7 @@ library(cowplot)
 library(mgcv)
 library(moments)
 library(patchwork)
-
-#library(scales)
+library(scales)
 
 # bring logger soil moisture files
 your_sm  <- read.csv ("data/all_data_daily.csv")
@@ -31,7 +30,7 @@ your_palette <- colorRampPalette(c("#000099", "#0000FF", "#5000FF", "#9F0FF0", "
 # plot
 fig_sd = your_sm %>% group_by(id_code, area) %>% 
   summarise(sm_mean = mean(moist_mean, na.rm = T),
-            sm_sd = sd(moist_mean, na.rm = T),
+            sm_sd = log(sd(moist_mean, na.rm = T)),
             sm_cv = sm_sd/sm_mean,
             sm_skew = skewness(moist_mean, na.rm = T)) %>% 
   ggplot(aes(x=sm_mean, y=sm_sd)) +
@@ -40,7 +39,7 @@ fig_sd = your_sm %>% group_by(id_code, area) %>%
   
   scale_fill_manual(values = rev(your_palette(7))) +
   scale_color_manual(values = rev(your_palette(7))) +
-  ggtitle ("Standard deviation") +
+  ggtitle ("log(Standard deviation)") +
   ylab ("") +
   xlab ("Mean soil moisture") +
   theme_cowplot(12) +
@@ -90,7 +89,7 @@ fig_sd + fig_cv + fig_skew + plot_layout(guides = "collect")
 
 fig_sd_all = your_sm %>% group_by(id_code, area) %>% 
   summarise(sm_mean = mean(moist_mean, na.rm = T),
-            sm_sd = sd(moist_mean, na.rm = T),
+            sm_sd = log(sd(moist_mean, na.rm = T)),
             sm_cv = sm_sd/sm_mean,
             sm_skew = skewness(moist_mean, na.rm = T)) %>% 
   ggplot(aes(x=sm_mean, y=sm_sd)) +
@@ -99,7 +98,7 @@ fig_sd_all = your_sm %>% group_by(id_code, area) %>%
   
   scale_fill_manual(values = rev(your_palette(7))) +
   scale_color_manual(values = rev(your_palette(7))) +
-  ggtitle ("Standard deviation") +
+  ggtitle ("log(Standard deviation)") +
   ylab ("") +
   xlab ("Mean soil moisture") +
   theme_cowplot(12) +
@@ -141,3 +140,53 @@ fig_skew_all = your_sm %>% group_by(id_code, area) %>%
 
 fig_sd_all + fig_cv_all + fig_skew_all + plot_layout(guides = "collect")
 
+your_sm %>% group_by(id_code, area) %>% 
+  summarise(sm_mean = mean(moist_mean, na.rm = T),
+            sm_sd = sd(moist_mean, na.rm = T),
+            sm_cv = sm_sd/sm_mean,
+            sm_skew = skewness(moist_mean, na.rm = T)) %>% 
+  ungroup() %>% 
+  select(-id_code, -area) %>% cor(., use = "pairwise.complete.obs")
+
+
+
+
+
+your_sm %>% group_by(id_code, area) %>% 
+  summarise(sm_mean = mean(moist_mean, na.rm = T),
+            sm_sd = sd(moist_mean, na.rm = T),
+            sm_cv = sm_sd/sm_mean,
+            sm_skew = skewness(moist_mean, na.rm = T)) %>% 
+  ungroup() %>% 
+  select(sm_mean, sm_sd) %>% 
+  filter(complete.cases(.)) %>% 
+  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() %>% as.data.frame()
+
+your_sm %>% group_by(id_code, area) %>% 
+  summarise(sm_mean = mean(moist_mean, na.rm = T),
+            sm_sd = sd(moist_mean, na.rm = T),
+            sm_cv = sm_sd/sm_mean,
+            sm_skew = skewness(moist_mean, na.rm = T)) %>% 
+  ungroup() %>% 
+  select(sm_mean) %>% 
+  filter(complete.cases(.)) %>% 
+  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() %>% as.data.frame()
+
+
+
+your_sm %>% group_by(id_code, area) %>% 
+  summarise(sm_mean = mean(moist_mean, na.rm = T),
+            sm_sd = log(sd(moist_mean, na.rm = T)),
+            sm_cv = sm_sd/sm_mean,
+            sm_skew = skewness(moist_mean, na.rm = T)) %>% 
+  ggplot(aes(x=sm_mean, y=sm_sd)) +
+  geom_point(aes(colour=area, size = 3), alpha=8/10) +
+  geom_smooth(method = gam, formula = y ~ s(x, k=4), se = T, colour="black") +
+  
+  scale_fill_manual(values = rev(your_palette(7))) +
+  scale_color_manual(values = rev(your_palette(7))) +
+  ggtitle ("Standard deviation") +
+  ylab ("") +
+  xlab ("Mean soil moisture") +
+  theme_cowplot(12) +
+  theme (aspect.ratio = 1, legend.title = element_blank())
