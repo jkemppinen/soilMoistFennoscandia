baf40aaf38c4eedf1f3a7750aee2afb00c714658
jkemppinen <julia.kemppinen@gmail.com>
2021-12-10 11:21:09 +0100

    climate distance comparison

--- scripts/calculate_pairwise_distances.R
+++ scripts/calculate_pairwise_distances.R
@@ -91,60 +91,78 @@ d %>% column_to_rownames("site") %>%
 xy <- t(combn(colnames(dm), 2))
 dm_geo <- data.frame(xy, geo_dist=dm[xy])
 
-# swi
+# climatic distance (ERA5 prec and temp)
 d %>% column_to_rownames("site") %>% 
-  select(swi) %>% 
+  select(mean_2m_temperature, sum_total_precipitation) %>% 
+  filter(complete.cases(.)) %>% 
+  dist() %>% as.matrix() -> dm
+
+xy <- t(combn(colnames(dm), 2))
+dm_cli <- data.frame(xy, cli_dist=dm[xy])
+
+# topographical distance (swi, ele)
+d %>% column_to_rownames("site") %>% 
+  select(altitude, swi) %>% 
   filter(complete.cases(.)) %>% 
   mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
 
 xy <- t(combn(colnames(dm), 2))
-dm2 <- data.frame(xy, swi=dm[xy])
+dm_top <- data.frame(xy, top_dist=dm[xy])
 
-# allwet_prop_10m
-d %>% column_to_rownames("site") %>% 
-  select(allwet_prop_10m) %>% 
-  filter(complete.cases(.)) %>% 
-  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
-
-xy <- t(combn(colnames(dm), 2))
-dm3 <- data.frame(xy, allwet_prop_10m=dm[xy])
-
-# fluvial_effect
-d %>% column_to_rownames("site") %>% 
-  select(fluvial_effect) %>% 
-  filter(complete.cases(.)) %>% 
-  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
-
-xy <- t(combn(colnames(dm), 2))
-dm4 <- data.frame(xy, fluvial_effect=dm[xy])
-
-# canopy_portion_conif
-d %>% column_to_rownames("site") %>% 
-  select(canopy_portion_conif) %>% 
-  filter(complete.cases(.)) %>% 
-  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
-
-xy <- t(combn(colnames(dm), 2))
-dm5 <- data.frame(xy, canopy_portion_conif=dm[xy])
+## swi
+#d %>% column_to_rownames("site") %>% 
+#  select(swi) %>% 
+#  filter(complete.cases(.)) %>% 
+#  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
+#
+#xy <- t(combn(colnames(dm), 2))
+#topo <- data.frame(xy, swi=dm[xy])
+#
+## allwet_prop_10m
+#d %>% column_to_rownames("site") %>% 
+#  select(allwet_prop_10m) %>% 
+#  filter(complete.cases(.)) %>% 
+#  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
+#
+#xy <- t(combn(colnames(dm), 2))
+#dm3 <- data.frame(xy, allwet_prop_10m=dm[xy])
+#
+## fluvial_effect
+#d %>% column_to_rownames("site") %>% 
+#  select(fluvial_effect) %>% 
+#  filter(complete.cases(.)) %>% 
+#  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
+#
+#xy <- t(combn(colnames(dm), 2))
+#dm4 <- data.frame(xy, fluvial_effect=dm[xy])
+#
+## canopy_portion_conif
+#d %>% column_to_rownames("site") %>% 
+#  select(canopy_portion_conif) %>% 
+#  filter(complete.cases(.)) %>% 
+#  mutate(across(everything(), ~rescale(.x))) %>% dist() %>% as.matrix() -> dm
+#
+#xy <- t(combn(colnames(dm), 2))
+#dm5 <- data.frame(xy, canopy_portion_conif=dm[xy])
 
 names(d)
 
 # Combine the pairwise distance dataframes
 
-all <- full_join(dm1, dm2) %>% 
-  full_join(., dm3) %>% 
-  full_join(., dm4) %>% 
-  full_join(., dm5) %>% 
+all <- full_join(dm1, dm_top) %>% 
+  full_join(., dm_cli) %>% 
   full_join(., dm_geo)
 
 names (all)
 
-cor(all$moist, all$canopy_portion_conif, use = "pairwise.complete.obs")
-cor(all[,3:7], use = "pairwise.complete.obs", method = "spearman")
-cor(all[,3:8] %>% mutate(geo_dist = sqrt(geo_dist)), use = "pairwise.complete.obs", method = "spearman")
+cor(all$moist, all$top_dist, use = "pairwise.complete.obs")
+cor(all[,3:5], use = "pairwise.complete.obs", method = "spearman")
+cor(all[,3:6] %>% mutate(geo_dist = sqrt(geo_dist)), use = "pairwise.complete.obs", method = "spearman")
+
 # lm
-lm.moist_base <- lm(moist ~ swi + allwet_prop_10m + fluvial_effect + canopy_portion_conif, data =all)
-lm.moist_full <- lm(moist ~ swi + allwet_prop_10m + fluvial_effect + canopy_portion_conif + sqrt(geo_dist), data =all)
+lm.moist_full <- lm(moist ~ swi +
+                      sqrt(cli_dist) +
+                      sqrt(geo_dist), data =all)
 
 anova(lm.moist)
 summary(lm.moist)
@@ -156,62 +174,56 @@ plot(lm.moist, las = 1)      # Residuals, Fitted, ...
 par(opar)
 
 plot_swi <- all %>% 
-  ggplot(aes(x=moist, y=swi)) +
+  ggplot(aes(x=moist, y=top_dist)) +
   geom_point(aes(size = 0.5), alpha=1/10) +
-  geom_smooth(method = gam, formula = y ~ s(x, k=4), se = T, colour="red") +
+  geom_smooth(method = gam, formula = y ~ s(x, k=3), se = T, colour="red") +
   
-  ggtitle ("Distance in relation to swi") +
+  ggtitle ("SWI distance") +
   ylab ("") +
-  xlab ("Distance in relation to mean and SD") +
+  xlab ("Mean-SD soil moisture distance") +
   theme_cowplot(12) +
   theme (aspect.ratio = 1, legend.title = element_blank())
 
-plot_allwet_prop_10m <- all %>% 
-  ggplot(aes(x=moist, y=allwet_prop_10m)) +
+plot_cli_dist <- all %>% 
+  ggplot(aes(x=moist, y=cli_dist)) +
   geom_point(aes(size = 0.5), alpha=1/10) +
-  geom_smooth(method = gam, formula = y ~ s(x, k=4), se = T, colour="red") +
+  geom_smooth(method = gam, formula = y ~ s(x, k=3), se = T, colour="red") +
   
-  ggtitle ("Distance in relation to allwet_prop_10m") +
+  ggtitle ("Climatic distance") +
   ylab ("") +
-  xlab ("Distance in relation to mean and SD") +
-  theme_cowplot(12) +
-  theme (aspect.ratio = 1, legend.title = element_blank())
-
-plot_fluvial_effect <- all %>% 
-  ggplot(aes(x=moist, y=fluvial_effect)) +
-  geom_point(aes(size = 0.5), alpha=1/10) +
-  geom_smooth(method = gam, formula = y ~ s(x, k=4), se = T, colour="red") +
-  
-  ggtitle ("Distance in relation to fluvial_effect") +
-  ylab ("") +
-  xlab ("Distance in relation to mean and SD") +
-  theme_cowplot(12) +
-  theme (aspect.ratio = 1, legend.title = element_blank())
-
-plot_canopy_portion_conif <- all %>% 
-  ggplot(aes(x=moist, y=canopy_portion_conif)) +
-  geom_point(aes(size = 0.5), alpha=1/10) +
-  geom_smooth(method = gam, formula = y ~ s(x, k=4), se = T, colour="red") +
-  
-  ggtitle ("Distance in relation to canopy_portion_conif") +
-  ylab ("") +
-  xlab ("Distance in relation to mean and SD") +
+  xlab ("Mean-SD soil moisture distance") +
   theme_cowplot(12) +
   theme (aspect.ratio = 1, legend.title = element_blank())
 
 plot_geo_dist <- all %>% 
-  ggplot(aes(x=moist, y=canopy_portion_conif)) +
+  ggplot(aes(x=moist, y=geo_dist)) +
   geom_point(aes(size = 0.5), alpha=1/10) +
-  geom_smooth(method = gam, formula = y ~ s(x, k=4), se = T, colour="red") +
+  geom_smooth(method = gam, formula = y ~ s(x, k=3), se = T, colour="red") +
   
-  ggtitle ("Distance in relation to geo_dist") +
+  ggtitle ("Geographical distance") +
   ylab ("") +
-  xlab ("Distance in relation to mean and SD") +
+  xlab ("Mean-SD soil moisture distance") +
   theme_cowplot(12) +
   theme (aspect.ratio = 1, legend.title = element_blank())
 
-(plot_swi | plot_allwet_prop_10m) / (plot_fluvial_effect | plot_canopy_portion_conif) + plot_layout(guides = "collect")
+plot_swi + plot_cli_dist + plot_geo_dist + plot_layout(guides = "collect")
 
-gam_moist <- gam(moist~s(swi, k = 4)+s(allwet_prop_10m, k = 4)+s(plot_fluvial_effect, k = 4)+s(plot_canopy_portion_conif, k = 4),data=all)
+gam_moist <- gam(moist~
+                   s(top_dist, k = 3)+
+                   s(cli_dist, k = 3)+
+                   s(geo_dist, k = 3), data=all)
 summary(gam_moist)
 
+names (d)
+
+d %>% 
+  ggplot(aes(x=sm_mean, y=sm_sd)) +
+  geom_point(aes(size = 0.5), alpha=1/10)
+
+d %>% 
+  ggplot(aes(x=x_utm, y=y_utm)) +
+  geom_point(aes(size = 0.5), alpha=1/10)
+
+d %>% 
+  ggplot(aes(x=sum_total_precipitation, y=mean_2m_temperature)) +
+  geom_point(aes(size = 0.5), alpha=1/10)
