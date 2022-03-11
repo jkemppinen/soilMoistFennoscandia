02e313d68ba5682519c4d3018cccf913d22e8ca3
jkemppinen <julia.kemppinen@gmail.com>
2022-03-11 11:43:16 +0100

    new figure for appendix: trends over time: mean and spatial heterogeneity

--- scripts/fig_spatiotempo_variation.R
+++ scripts/fig_spatiotempo_variation.R
@@ -87,64 +87,64 @@ your_sm %>% group_by(site, area) %>%
             sm_quantile25 = quantile(sm_sd, 0.25, na.rm = T),
             sm_quantile75 = quantile(sm_sd, 0.75, na.rm = T)) -> temporal_data
 
-# plot spatial vs temporal sd in one plot - line
-p_sd_all_line = full_join (spatial_data, temporal_data)  %>% 
-  ggplot(aes(x=date, group = area)) +
-  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
-  geom_line(aes(y=sm_sd, colour=area), size=0.5) +
-  scale_fill_manual(values = rev(your_palette(7))) +
-  scale_color_manual(values = rev(your_palette(7))) +
-  ylab ("Standard deviation") +
-  xlab ("Time") +
-  ylim (0, 21) +
-  scale_x_date(date_breaks = "2 month", date_labels =  "%d.%m.") +
-  theme_classic() +
-  theme(
-    aspect.ratio = 1,
-    legend.position = "None")
-
-# plot area names only
-your_palette(7)
-
-p_names = ggplot() +
-  annotate("text", x = 1, y =1.7, size = 3.5, fontface =2,
-           label = "Rásttigáisá",
-           colour="#FFDB24") +
-  annotate("text", x = 1, y =1.6, size = 3.5, fontface =2,
-           label = "Kilpisjärvi",
-           colour="#FF9F5F") +
-  annotate("text", x = 1, y =1.5, size = 3.5, fontface =2,
-           label = "Värriö",
-           colour="#F9649B") +
-  annotate("text", x = 1, y =1.4, size = 3.5, fontface =2,
-           label = "Tiilikka",
-           colour="#C728D6") +
-  annotate("text", x = 1, y =1.3, size = 3.5, fontface =2,
-           label = "Pisa",
-           colour="#6A05FA") +
-  annotate("text", x = 1, y =1.2, size = 3.5, fontface =2,
-           label = "Hyytiälä",
-           colour="#0D00FF") +
-  annotate("text", x = 1, y =1.1, size = 3.5, fontface =2,
-           label = "Karkali",
-           colour="#000099") +
-  theme_void()
-
-# print pdf
-layout <- '
-AA
-AA
-B#
-
-'
-
-dev.off()
-pdf(file="fig/fig_sd.pdf", width = 3.54, height = 5)
-
-wrap_plots(A = p_sd_all_line,
-           B = p_names, design = layout)
-
-dev.off()
+## plot spatial vs temporal sd in one plot - line
+#p_sd_all_line = full_join (spatial_data, temporal_data)  %>% 
+#  ggplot(aes(x=date, group = area)) +
+#  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
+#  geom_line(aes(y=sm_sd, colour=area), size=0.5) +
+#  scale_fill_manual(values = rev(your_palette(7))) +
+#  scale_color_manual(values = rev(your_palette(7))) +
+#  ylab ("Standard deviation") +
+#  xlab ("Time") +
+#  ylim (0, 21) +
+#  scale_x_date(date_breaks = "2 month", date_labels =  "%d.%m.") +
+#  theme_classic() +
+#  theme(
+#    aspect.ratio = 1,
+#    legend.position = "None")
+#
+## plot area names only
+#your_palette(7)
+#
+#p_names = ggplot() +
+#  annotate("text", x = 1, y =1.7, size = 3.5, fontface =2,
+#           label = "Rásttigáisá",
+#           colour="#FFDB24") +
+#  annotate("text", x = 1, y =1.6, size = 3.5, fontface =2,
+#           label = "Kilpisjärvi",
+#           colour="#FF9F5F") +
+#  annotate("text", x = 1, y =1.5, size = 3.5, fontface =2,
+#           label = "Värriö",
+#           colour="#F9649B") +
+#  annotate("text", x = 1, y =1.4, size = 3.5, fontface =2,
+#           label = "Tiilikka",
+#           colour="#C728D6") +
+#  annotate("text", x = 1, y =1.3, size = 3.5, fontface =2,
+#           label = "Pisa",
+#           colour="#6A05FA") +
+#  annotate("text", x = 1, y =1.2, size = 3.5, fontface =2,
+#           label = "Hyytiälä",
+#           colour="#0D00FF") +
+#  annotate("text", x = 1, y =1.1, size = 3.5, fontface =2,
+#           label = "Karkali",
+#           colour="#000099") +
+#  theme_void()
+#
+## print pdf
+#layout <- '
+#AA
+#AA
+#B#
+#
+#'
+#
+#dev.off()
+#pdf(file="fig/fig_sd.pdf", width = 3.54, height = 5)
+#
+#wrap_plots(A = p_sd_all_line,
+#           B = p_names, design = layout)
+#
+#dev.off()
 
 
 # plot spatial vs temporal sd in one plot - smooth
@@ -154,7 +154,7 @@ p_mean_all_smooth = mean_data  %>%
   geom_smooth(aes (y=sm_mean, colour=area, fill=area), size=0.5, method = "lm", se = T, alpha=3/10) +
   scale_fill_manual(values = rev(your_palette(7))) +
   scale_color_manual(values = rev(your_palette(7))) +
-  ylab ("") +
+  ylab ("Mean VWC%") +
   xlab ("Time") +
   ylim (0, 45) +
   scale_x_date(date_breaks = "1 month", date_labels =  "%d.%m.") +
@@ -168,64 +168,7 @@ p_sd_all_smooth = spatial_data  %>%
   geom_smooth(aes (y=sm_sd, colour=area, fill=area), size=0.5, method = "lm", se = T, alpha=3/10) +
   scale_fill_manual(values = rev(your_palette(7))) +
   scale_color_manual(values = rev(your_palette(7))) +
-  ylab ("") +
-  xlab ("Time") +
-  ylim (0, 21) +
-  scale_x_date(date_breaks = "1 month", date_labels =  "%d.%m.") +
-  theme_classic() +
-  theme(
-    aspect.ratio = 1,
-    legend.position = "None")
-
-
-# EXTRA FOR APPENDIX?
-
-# plot spatial vs temporal sd
-p_sd = full_join (spatial_data, temporal_data)  %>% 
-  ggplot(aes(x=date, group = area)) +
-  geom_ribbon(aes(ymin = sm_quantile25, ymax = sm_quantile75, fill = area), alpha=3/10)+
-  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
-  geom_line(aes(y=sm_sd, colour=area), size=0.5) +
-  geom_smooth(aes (y=sm_sd, colour=area, fill=area), size=0.5, method = "lm", se = T, alpha=3/10) +
-  scale_fill_manual(values = rev(your_palette(7))) +
-  scale_color_manual(values = rev(your_palette(7))) +
-  ylab ("Standard deviation") +
-  xlab ("Time") +
-  scale_x_date(date_breaks = "2 month", date_labels =  "%d.%m.") +
-  theme_classic() +
-  theme(
-    aspect.ratio = 1,
-    legend.position = "None") +
-  #facet_grid(facets = area ~ .) +
-  facet_wrap(vars(area), ncol= 1) +
-  theme(
-    strip.background = element_blank(),
-    strip.text.x = element_blank())
-
-# plot spatial vs temporal sd in one plot - line
-p_sd_all_line = full_join (spatial_data, temporal_data)  %>% 
-  ggplot(aes(x=date, group = area)) +
-  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
-  geom_line(aes(y=sm_sd, colour=area), size=0.5) +
-  scale_fill_manual(values = rev(your_palette(7))) +
-  scale_color_manual(values = rev(your_palette(7))) +
-  ylab ("") +
-  xlab ("") +
-  ylim (0, 21) +
-  theme_classic() +
-  theme(
-    axis.text.x = element_blank(),
-    aspect.ratio = 1,
-    legend.position = "None")
-
-# plot spatial vs temporal sd in one plot - smooth
-p_sd_all_smooth = full_join (spatial_data, temporal_data)  %>% 
-  ggplot(aes(x=date, group = area)) +
-  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
-  geom_smooth(aes (y=sm_sd, colour=area, fill=area), size=0.5, method = "lm", se = T, alpha=3/10) +
-  scale_fill_manual(values = rev(your_palette(7))) +
-  scale_color_manual(values = rev(your_palette(7))) +
-  ylab ("") +
+  ylab ("Spatial heterogeneity\n(Standard deviation VWC%)") +
   xlab ("Time") +
   ylim (0, 21) +
   scale_x_date(date_breaks = "1 month", date_labels =  "%d.%m.") +
@@ -248,10 +191,10 @@ p_names = ggplot() +
            label = "Värriö",
            colour="#F9649B") +
   annotate("text", x = 1, y =1.4, size = 3.5, fontface =2,
-           label = "Pisa",
+           label = "Tiilikka",
            colour="#C728D6") +
   annotate("text", x = 1, y =1.3, size = 3.5, fontface =2,
-           label = "Tiilikka",
+           label = "Pisa",
            colour="#6A05FA") +
   annotate("text", x = 1, y =1.2, size = 3.5, fontface =2,
            label = "Hyytiälä",
@@ -263,23 +206,125 @@ p_names = ggplot() +
 
 # print pdf
 layout <- '
-ABBB
-ABBB
-ABBB
-ACCC
-ACCC
-ACCC
-ADDD
+AB
+AB
+CC
 '
 
 dev.off()
-pdf(file="fig/fig_sd.pdf", width = 7.48, height = 8.66)
+pdf(file="fig/fig_app.pdf", width = 7.48, height = 5)
 
-wrap_plots(A = p_sd,
-           B = p_sd_all_line,
-           C = p_sd_all_smooth,
-           D = p_names, design = layout) +
-  plot_annotation(tag_levels = 'A') & 
-  theme(plot.tag = element_text(size = 8))
+wrap_plots(A = p_mean_all_smooth,
+           B = p_sd_all_smooth,
+           C = p_names, design = layout)
 
 dev.off()
+
+spatial_data %>% group_by(area) %>% 
+  summarise(mean(sm_sd, na.rm = T))
+
+## EXTRA FOR APPENDIX?
+#
+## plot spatial vs temporal sd
+#p_sd = full_join (spatial_data, temporal_data)  %>% 
+#  ggplot(aes(x=date, group = area)) +
+#  geom_ribbon(aes(ymin = sm_quantile25, ymax = sm_quantile75, fill = area), alpha=3/10)+
+#  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
+#  geom_line(aes(y=sm_sd, colour=area), size=0.5) +
+#  geom_smooth(aes (y=sm_sd, colour=area, fill=area), size=0.5, method = "lm", se = T, alpha=3/10) +
+#  scale_fill_manual(values = rev(your_palette(7))) +
+#  scale_color_manual(values = rev(your_palette(7))) +
+#  ylab ("Standard deviation") +
+#  xlab ("Time") +
+#  scale_x_date(date_breaks = "2 month", date_labels =  "%d.%m.") +
+#  theme_classic() +
+#  theme(
+#    aspect.ratio = 1,
+#    legend.position = "None") +
+#  #facet_grid(facets = area ~ .) +
+#  facet_wrap(vars(area), ncol= 1) +
+#  theme(
+#    strip.background = element_blank(),
+#    strip.text.x = element_blank())
+#
+## plot spatial vs temporal sd in one plot - line
+#p_sd_all_line = full_join (spatial_data, temporal_data)  %>% 
+#  ggplot(aes(x=date, group = area)) +
+#  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
+#  geom_line(aes(y=sm_sd, colour=area), size=0.5) +
+#  scale_fill_manual(values = rev(your_palette(7))) +
+#  scale_color_manual(values = rev(your_palette(7))) +
+#  ylab ("") +
+#  xlab ("") +
+#  ylim (0, 21) +
+#  theme_classic() +
+#  theme(
+#    axis.text.x = element_blank(),
+#    aspect.ratio = 1,
+#    legend.position = "None")
+#
+## plot spatial vs temporal sd in one plot - smooth
+#p_sd_all_smooth = full_join (spatial_data, temporal_data)  %>% 
+#  ggplot(aes(x=date, group = area)) +
+#  geom_line(aes(y=sm_median, colour=area), size=0.5, linetype=2) +
+#  geom_smooth(aes (y=sm_sd, colour=area, fill=area), size=0.5, method = "lm", se = T, alpha=3/10) +
+#  scale_fill_manual(values = rev(your_palette(7))) +
+#  scale_color_manual(values = rev(your_palette(7))) +
+#  ylab ("") +
+#  xlab ("Time") +
+#  ylim (0, 21) +
+#  scale_x_date(date_breaks = "1 month", date_labels =  "%d.%m.") +
+#  theme_classic() +
+#  theme(
+#    aspect.ratio = 1,
+#    legend.position = "None")
+#
+## plot area names only
+#your_palette(7)
+#
+#p_names = ggplot() +
+#  annotate("text", x = 1, y =1.7, size = 3.5, fontface =2,
+#           label = "Rásttigáisá",
+#           colour="#FFDB24") +
+#  annotate("text", x = 1, y =1.6, size = 3.5, fontface =2,
+#           label = "Kilpisjärvi",
+#           colour="#FF9F5F") +
+#  annotate("text", x = 1, y =1.5, size = 3.5, fontface =2,
+#           label = "Värriö",
+#           colour="#F9649B") +
+#  annotate("text", x = 1, y =1.4, size = 3.5, fontface =2,
+#           label = "Pisa",
+#           colour="#C728D6") +
+#  annotate("text", x = 1, y =1.3, size = 3.5, fontface =2,
+#           label = "Tiilikka",
+#           colour="#6A05FA") +
+#  annotate("text", x = 1, y =1.2, size = 3.5, fontface =2,
+#           label = "Hyytiälä",
+#           colour="#0D00FF") +
+#  annotate("text", x = 1, y =1.1, size = 3.5, fontface =2,
+#           label = "Karkali",
+#           colour="#000099") +
+#  theme_void()
+#
+## print pdf
+#layout <- '
+#ABBB
+#ABBB
+#ABBB
+#ACCC
+#ACCC
+#ACCC
+#ADDD
+#'
+#
+#dev.off()
+#pdf(file="fig/fig_sd.pdf", width = 7.48, height = 8.66)
+#
+#wrap_plots(A = p_sd,
+#           B = p_sd_all_line,
+#           C = p_sd_all_smooth,
+#           D = p_names, design = layout) +
+#  plot_annotation(tag_levels = 'A') & 
+#  theme(plot.tag = element_text(size = 8))
+#
+#dev.off()
